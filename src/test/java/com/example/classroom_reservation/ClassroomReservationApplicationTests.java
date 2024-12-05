987b7b1fba8f2d40aeb528244e2c8c506267b489
Saangjun00 <149948808+Saangjun00@users.noreply.github.com>
2024-12-05 21:03:41 +0900

    Initial commit

---
+++ src/test/java/com/example/classroom_reservation/ClassroomReservationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.classroom_reservation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClassroomReservationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
